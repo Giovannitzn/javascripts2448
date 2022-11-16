c5a0a17d722c83a34d026752099c4942fa3a33ab
Giovanni Tzn <giotz2020@gmail.com>
2022-11-15 21:51:10 -0500

    Usando condicionales

---
+++ js/operador.js
@@ -0,0 +1,17 @@
+let numero1 = prompt("Ingrese el numero");
+
+if (numero1>1000){
+    alert ("Mayor a 1000");
+}else {alert ("Es menor"); }
+
+let palabra = prompt("Ingrese texto");
+
+if (palabra =="Hola"){
+    alert ("Hola");
+}else {alert ("diferente"); }
+
+let numero2 = prompt("Ingrese el numero");
+
+if (numero2>10 && numero2<50) {
+    alert ("numero esta en rango de 10 y 50");
+}else { alert("fuera de rango");}
